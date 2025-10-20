5c3be95f758f683f67f2591d62ce2ed231188507
Debora Amaral <debora.amaral@sejaefi.com.br>
2025-10-20 15:33:59 -0300

    ✨ Exemplo para Configurar o webhook de cobrança de Pix Automático

---
+++ examples/pix/webhooks/automatic/pixConfigWebhookRecurrenceAutomatic.rb
@@ -0,0 +1,19 @@
+require 'sdk_ruby_apis_efi'
+require_relative "../../credentials"
+
+options = {
+  client_id: CREDENTIALS::CLIENT_ID,
+  client_secret: CREDENTIALS::CLIENT_SECRET,
+  sandbox: CREDENTIALS::SANDBOX
+}
+
+headers = {
+  'x-skip-mtls-checking': 'true'
+}
+
+body = {  
+    webhookUrl: 'https://usuario.recebedor.com/api/webhookrec/'
+}
+
+efipay = SdkRubyApisEfi.new(options)
+puts efipay.pixConfigWebhookRecurrenceAutomatic(headers: headers, body: body)
