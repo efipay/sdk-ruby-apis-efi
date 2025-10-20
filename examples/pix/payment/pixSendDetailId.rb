5ed25312c513f13e0fdf29e3322027006eb6ee26
Debora Amaral <debora.amaral@sejaefi.com.br>
2025-10-20 15:44:19 -0300

    ✨ Exemplo para consultar detalhes de Pix enviado a partir do id

---
+++ examples/pix/payment/pixSendDetailId.rb
@@ -0,0 +1,21 @@
+require 'sdk_ruby_apis_efi'
+require_relative "../../credentials"
+
+require "uri"
+require "net/http"
+require "openssl"
+
+options = {
+  client_id: CREDENTIALS::CLIENT_ID,
+  client_secret: CREDENTIALS::CLIENT_SECRET,
+  certificate: CREDENTIALS::CERTIFICATE,
+  sandbox: CREDENTIALS::SANDBOX
+}
+
+params = {
+    idEnvio: "0249756789163473800"
+}
+
+efipay = SdkRubyApisEfi.new(options)
+puts efipay.pixSendDetailId(params: params)
+
