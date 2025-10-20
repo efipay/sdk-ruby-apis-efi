f7ea72be10a7766922ae50f4947938368dd3e915
Debora Amaral <debora.amaral@sejaefi.com.br>
2025-10-20 15:15:51 -0300

    ✨ Exemplo para efetuar uma devolução de um pagamento agendado de Pix via Open Finance

---
+++ examples/open-finance/payments/schedule/ofDevolutionSchedulePix.rb
@@ -0,0 +1,19 @@
+require 'sdk_ruby_apis_efi'
+require_relative "../../credentials"
+
+options = {
+  client_id: CREDENTIALS::CLIENT_ID,
+  client_secret: CREDENTIALS::CLIENT_SECRET,
+  sandbox: CREDENTIALS::SANDBOX
+}
+
+params = {
+  identificadorPagamento: 'urn:teste'
+}
+
+body = {
+    valor: '0.01'
+}
+
+efipay = SdkRubyApisEfi.new(options)
+puts efipay.ofDevolutionSchedulePix(params: params, body: body)
