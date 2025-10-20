a0f88c5265c07bf79dcb389dab93cb8e385127b2
Debora Amaral <debora.amaral@sejaefi.com.br>
2025-10-20 15:33:09 -0300

    📁 Reorganiza arquivos em subpastas por tipo

---
+++ examples/pix/webhooks/cob-cobv/pixDeleteWebhook.rb
@@ -0,0 +1,22 @@
+require 'sdk_ruby_apis_efi'
+require_relative "../../credentials"
+
+require "uri"
+require "net/http"
+require "openssl"
+
+
+options = {
+  client_id: CREDENTIALS::CLIENT_ID,
+  client_secret: CREDENTIALS::CLIENT_SECRET,
+  certificate: CREDENTIALS::CERTIFICATE,
+  sandbox: CREDENTIALS::SANDBOX
+}
+
+params = {
+    chave: "7f4151c7-49a5-4e95-ab24-55bc8f1a2e86"
+}
+
+efipay = SdkRubyApisEfi.new(options)
+puts efipay.pixDeleteWebhook(params: params)
+
