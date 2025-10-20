57ceca313920652ba47be431a1f955afbadc9020
Debora Amaral <debora.amaral@sejaefi.com.br>
2025-10-20 15:27:06 -0300

    ✨ Exemplo para Consultar lista de cobranças de Pix Automático

---
+++ examples/pix/automatic/cobr/pixUpdateAutomaticCharge.rb
@@ -0,0 +1,19 @@
+require 'sdk_ruby_apis_efi'
+require_relative "../../credentials"
+
+options = {
+  client_id: CREDENTIALS::CLIENT_ID,
+  client_secret: CREDENTIALS::CLIENT_SECRET,
+  sandbox: CREDENTIALS::SANDBOX
+}
+
+params = {
+    txid: 'fc9a4366jf3d4964b5dba6c91a8722d3'
+}
+
+body = {
+    status: 'CANCELADA'
+}
+
+efipay = SdkRubyApisEfi.new(options)
+puts efipay.pixUpdateAutomaticCharge(params: params, body: body)
